723038799833afa08fdd7aa351141769ab351bde
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-11-29 16:41:54 +0000

    Add app.json for automated deployment to Heroku

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'welcome/$', TemplateView.as_view(template_name='welcome.html')),
     url(
         r'^admin/',
         include(admin.site.urls)
