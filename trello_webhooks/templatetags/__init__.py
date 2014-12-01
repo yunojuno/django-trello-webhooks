ffa4fe69c2408a5c5aecf0acdec74bb5a0dc266b
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-12-01 21:37:35 +0000

    Add tests for templatetags

--- trello_webhooks/templatetags/__init__.py
+++ trello_webhooks/templatetags/__init__.py
@@ -1,1 +1,4 @@
 # trello_webhooks.templatetags package
+# do this to force coverage to include template_tags
+import trello_webhook_tags
+__all__ = ['trello_webhook_tags']
--- trello_webhooks/tests/test_templatetags.py
+++ trello_webhooks/tests/test_templatetags.py
@@ -1,1 +1,30 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
 
+from trello_webhooks.settings import TRELLO_API_KEY
+from trello_webhooks.templatetags.trello_webhook_tags import (
+    trello_api_key,
+    trello_updates
+)
+
+
+class TemplateTagTests(TestCase):
+
+    def test_trello_api_key(self):
+        self.assertEqual(trello_api_key(), TRELLO_API_KEY)
+
+    def test_trello_updates(self):
+        # good data:
+        old = {'pos': 1}
+        new = {'pos': 2, 'abc': 'xyz'}
+        self.assertEqual(
+            trello_updates(new, old),
+            {'pos': (1, 2)}
+        )
+
+        # bad data
+        new = {}
+        self.assertEqual(
+            trello_updates(new, old),
+            {'pos': (1, None)}
+        )
