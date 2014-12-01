c875661628c18b65f76ceb0f7f44f7d3ba6832f8
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-12-01 20:00:16 +0000

    Remove redundant welcome.html url and template
    It was added because of the Heroku deploy button instructions, but
    doesn't appear to be used? Also updated app.json.

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,18 +1,10 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'welcome/$', TemplateView.as_view(template_name='welcome.html')),
-    url(
-        r'^admin/',
-        include(admin.site.urls)
-    ),
-    url(
-        r'^webhooks/',
-        include('trello_webhooks.urls')
-    ),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^webhooks/', include('trello_webhooks.urls')),
 )
