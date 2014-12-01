6309031090a135856e6e2b3f8381202d6d17b72f
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-12-01 19:10:12 +0000

    Send unknown events to HipChat in red
    If an event comes in to the test_app and has no matching template,
    then send it to HipChat in red, not yellow, making it easier to
    manage.

--- test_app/signals.py
+++ test_app/signals.py
@@ -13,6 +13,15 @@ logger = logging.getLogger(__name__)
 
 @receiver(callback_received, dispatch_uid="callback_received")
 def on_callback_received(sender, **kwargs):
+    # if a template exists for the event_type, then send the output
+    # as a normal notification, in 'yellow'
+    # if no template exists, send a notification in 'red'
     event = kwargs.pop('event')
     if settings.HIPCHAT_ENABLED:
-        send_to_hipchat(event.render())
+        rendered = event.render()
+        color = "yellow" if rendered else "red"
+        html = rendered or (
+            u"No template available for '%s'"
+            % event.event_type
+        )
+        send_to_hipchat(html, color=color)
