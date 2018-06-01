cd78a2c1f666801de97d9827881ed4c70ad3f789
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-01 15:51:19 +0200

    Remove code style issues reported by flake8 from trello_webhooks/urls.py.
    Fixed wrong indentation in urlpatterns.

--- trello_webhooks/urls.py
+++ trello_webhooks/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from trello_webhooks import views
 
 urlpatterns = patterns(views,
-    url(
-        r'^(?P<auth_token>\w+)/(?P<trello_model_id>\w+)/$',
-        views.api_callback,
-        name="trello_callback_url"
-    ),
-)
+                       url(
+                           r'^(?P<auth_token>\w+)/(?P<trello_model_id>\w+)/$',
+                           views.api_callback,
+                           name="trello_callback_url"
+                       ),
+                       )
