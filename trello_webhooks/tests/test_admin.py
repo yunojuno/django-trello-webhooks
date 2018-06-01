322d95ca6a97c2c01b2b21935d6c83e2ad48a88c
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-06-01 15:15:25 +0200

    Test add_attachment_type admin method.
    Added test for add_attachment admin method in test_admin.py.

--- trello_webhooks/tests/test_admin.py
+++ trello_webhooks/tests/test_admin.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
+import mock
 from django.test import TestCase
 
 from trello_webhooks.admin import CallbackEventAdmin
 from trello_webhooks.models import Webhook, CallbackEvent
+from trello_webhooks.tests import get_sample_data
+
+
+class MockResponse():
+    def iter_content(self, chunk_size):
+        return [1, 2]
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def __enter__(self):
+        return self
 
 
 class CallbackEventAdminTests(TestCase):
@@ -30,3 +43,30 @@ class CallbackEventAdminTests(TestCase):
         self.assertIsNotNone(self.admin.rendered(self.event))
         self.event.event_type = "X"
         self.assertIsNone(self.admin.rendered(self.event))
+
+    @mock.patch('trello_webhooks.models.requests.get', return_value=MockResponse())
+    @mock.patch('trello_webhooks.models.magic.from_buffer', return_value='image/png')
+    @mock.patch('trello_webhooks.admin.logger')
+    def test_add_attachment_type(self, mock_logger, mock_from_buffer, mock_response):
+        mock_request = mock.Mock()
+        mock_request.user = 'test_user'
+        # Create a CallbackEvent object with attachment but no attachment type.
+        hook = Webhook().save(sync=False)
+        payload = get_sample_data('addAttachmentToCardImageType', 'json')
+        event = CallbackEvent(
+            webhook=hook,
+            event_type='addAttachmentToCard',
+            event_payload=payload
+        )
+        event.save()
+        queryset = CallbackEvent.objects.all()
+        # Run helper function to add the missing attachment type.
+        self.admin.add_attachment_type(mock_request, queryset)
+        # Reload event objects from the database after it was updated.
+        # Since we create one CallbackEvent object in setUp the id of the one we
+        # need to confirm was changed will be id=2
+        event = CallbackEvent.objects.get(id=2)
+        self.assertEqual(
+            event.event_payload['action']['data']['attachment']['attachmentType'], 'image/png')
+        mock_logger.info.assert_called_once_with(
+            '%s added attachment type to %i CallbackEvents from the admin site.', 'test_user', 1)
